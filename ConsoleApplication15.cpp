8efb0f6e1249b181a510bba6cca0794dc1bc1c0f
Sapsaq <coool.ranboo@gmail.com>
2025-11-19 12:07:58 +0300

    Add files via upload

---
+++ ConsoleApplication15.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Person
+{
+    string last_name;
+    string first_name;
+};
+
+vector <string> open_file(const string& path)
+{
+    ifstream file;
+    file.open(path);
+    vector <string> result;
+    string line;
+    while (getline(file, line))
+    {
+        result.push_back(line);
+    }
+    file.close();
+
+    return result;
+}
+
+void split(const string& line, char delim, Person )
+{
+
+}
+
+int main()
+{
+    
+}
